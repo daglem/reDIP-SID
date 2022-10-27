9c8b6b3cd22e2246712050c4538406002fee347b
Dag Lem <dag@nimrod.no>
2022-10-27 17:14:44 +0200

    And there was sound!
    * sv2v is used for preprocessing by default - there seems to be a
      SystemVerilog bug in Yosys, which causes only noise to be output.
    * Corrected timing for I2S, yielding clean sound output.
    * Temporarily put both SIDs at D400, pending runtime configuration.

--- gateware/i2s_dsp_mode.sv
+++ gateware/i2s_dsp_mode.sv
@@ -18,7 +18,7 @@
 
 // I2S SGTL5000 PCM Format A input / output.
 // Note that this implementation requires that I2S_LRCLK is driven high for
-// exactly one cycle.
+// exactly one I2S_SCLK cycle.
 module i2s_dsp_mode #(
     parameter  BITS = 64,
     localparam COUNTBITS = $clog2(BITS)
@@ -41,19 +41,18 @@ module i2s_dsp_mode #(
     logic i2s_dout;
     logic lrclk_prev = 0;
     logic sclk_prev  = 0;
-    logic sclk_rise  = 0;
     logic sclk_fall  = 0;
+    logic dout_prev  = 0;
     logic last_bit   = 0;
     logic [COUNTBITS-1:0] bitnum = 0;
 
-    // Registered inputs. Sample on the falling edge of clk,
-    // in order to detect edges as early as possible.
+    // Registered inputs.
     SB_IO #(
         .PIN_TYPE    (6'b0000_00)
     ) i2s_in[2:0] (
         .PACKAGE_PIN ({ pad_lrclk, pad_sclk, pad_dout }),
         .INPUT_CLK   (clk),
-        .D_IN_1      ({ i2s_lrclk, i2s_sclk, i2s_dout })
+        .D_IN_0      ({ i2s_lrclk, i2s_sclk, i2s_dout })
     );
 
     // Simple output, not registered.
@@ -64,23 +63,34 @@ module i2s_dsp_mode #(
         .OUTPUT_CLK  (clk),
         .D_OUT_0     (i2s_din)
     );
-    
+
+    always_comb begin
+        i2s_din = dout[BITS-1];
+    end
+
     always @(posedge clk) begin
         lrclk_prev <= i2s_lrclk;
         sclk_prev  <= i2s_sclk;
-        sclk_rise  <= ~sclk_prev &  i2s_sclk;
-        sclk_fall  <=  sclk_prev & ~i2s_sclk;
+        sclk_fall  <= sclk_prev & ~i2s_sclk;
+        dout_prev  <= i2s_dout;
 
         if (sclk_fall) begin
             if (lrclk_prev) begin
-                // I2S_LRCLK pulse; prepare for data exchange.
+                // I2S_LRCLK pulse; initiate data exchange and output first bit.
                 dout   <= audio_o;
                 bitnum <= 0;
             end else begin
                 bitnum <= bitnum + 1;
+
+                // Shift out next output bit.
+                // The SGTL5000 hold time is only 10ns, i.e. it will have read
+                // the previous I2S_DIN on the falling edge of I2S_SCLK by now.
+                // We can thus change I2S_DIN without first detecting the rising
+                // edge of I2S_SCLK (which may already be in the past).
+                dout <= { dout[BITS-2:0], 1'b0 };
             end
 
-            // Any last bit is processed even if I2S_LRCLK is high
+            // The last bit is processed even if I2S_LRCLK is high
             // (i.e. no padding, e.g. for 64 bits).
             last_bit <= (bitnum == BITS - 1);
 
@@ -89,10 +99,7 @@ module i2s_dsp_mode #(
             end
 
             // Shift in input data bit.
-            din <= { din[BITS-2:0], i2s_dout };
-        end else if (sclk_rise) begin
-            // Shift out output data bit.
-            { i2s_din, dout } <= { dout, 1'b0 };
+            din <= { din[BITS-2:0], dout_prev };
         end
     end
 endmodule
